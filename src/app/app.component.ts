d21d9a747aa98b2f4b9b532cdfbbca7a60061ef9
Shashank Rai <Shashank.R@hyderabad.india.orioninc.com>
2018-04-17 15:42:33 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: '<app-employee></app-employee>',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
